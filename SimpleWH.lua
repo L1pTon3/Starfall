401e01c1f9887d877a86137aef6446d93a82e1fa
L1pTon3 <56623516+L1pTon3@users.noreply.github.com>
2019-11-04 02:34:58 +0200

    Update SimpleWH.lua

--- SimpleWH.lua
+++ SimpleWH.lua
@@ -4,7 +4,7 @@
 
 hook.add("postdrawopaquerenderables",'',function()
     local players = find.allPlayers()
-    for i=1,#players do
+    for i=1, #players do
         render.setColor(team.getColor( players[i]:getTeam()))
         render.draw3DWireframeBox( players[i]:getPos(), Angle(),players[i]:obbCenter()-players[i]:obbSize(), players[i]:obbSize() )
     end
